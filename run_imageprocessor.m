cda5c752b88da5f2fa8af8e21ad75941ec0d18ec
Mustapha Bouhka <bouhka.mus@gmail.com>
2026-05-23 18:47:57 +0200

    Add files via upload

---
+++ run_imageprocessor.m
@@ -0,0 +1,19 @@
+clear;clc;close all;
+
+ip=imageprocessor();
+
+img=ip.load_image('test_1.png');
+stars=ip.detect_stars(img);
+mu = mean(img(:));
+sig = std(img(:));
+fprintf('mu=%.4f, sigma=%.4f\n',mu,sig);
+fprintf('threshold T= %.4f\n',mu+5*sig);
+
+fprintf('Detected Stars: %d\n', size(stars,1));
+fprintf('\n%-6s %-10s %-10s %-12s\n','Star','u[px]', 'v[px]','brightness');
+fprintf('%s\n',repmat('-', 1, 42)) %ripulisce la formattazione della tabella in console
+
+for i=1:size(stars,1)
+    fprintf('%-6d %-10.2f %-10.2f %-12.4f\n',i,stars(i,1),stars(i,2),stars(i,3));
+end
+ip.visualize(img,stars);
